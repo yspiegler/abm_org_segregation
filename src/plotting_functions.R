963ce70981a48c3cb280bc841c1014d789b32198
yspiegler <yspiegler@hbs.edu>
2024-07-19 15:00:23 -0400

    added a working save_plot_print (for print) function that takes a plot and a name for the file and saves it

--- src/plotting_functions.R
+++ src/plotting_functions.R
@@ -1,5 +1,6 @@
 library(ggplot2)
-library(geomtextpath)
+library(ggtext)
+
 
 ######################################################
 ######################################################
@@ -53,7 +54,7 @@ plot_batch_dissimilarity <- function(batch, agent_ratio_str) {
 }
 
 # plot simulation batch of any number as the change in Theil segregation score as a function of sim iterations 
-plot_batch_theil <- function(batch, agent_ratio_str) {
+plot_batch_theil <- function(batch, agent_ratio_str, preferences = TRUE, plot_all_sims = TRUE) {
   
   combined_df <- map2_df(batch, seq_along(batch), 
                          ~ .x %>%
@@ -72,31 +73,45 @@ plot_batch_theil <- function(batch, agent_ratio_str) {
   #calculate the % difference, round to 2 decimals, and add a '+' sign if the number is positive
   theil_diff <- str_replace(str_c(round(100*((theil_t1-theil_t0)/theil_t0), 2), "%"), "^(?=[0-9])", "+")
   
-  print(str_c(theil_diff, " <- diff, t0 -> ", theil_t0, " and theil t1 -> ", theil_t1))
+  pct_up <- mean(combined_df$theil_increased == "h_increased") * 100
+  pct_down <- 100 - pct_up
+  
+  if(preferences) {
+    pref_txt <- "based on racial composition preferences"  
+  } else {
+    pref_txt <- "NULL models (no compositional preferences)"
+  }
+  
+  
+  subtitle_str <- str_c(
+    "Based on ", length(batch), " simulations, each of ", nrow(batch[[1]]), 
+    " organizations with a ratio of ", agent_ratio_str, " White/Black agents.<br>",
+    "<span style='color:steelblue;'>**", pct_up, "% of simulations**", 
+    "</span> increased in segregation, while <span style='color:tomato;'>**",  
+    pct_down, "% of simulations</span> decreased**.<br>",
+    "**Overall, the mean change in segregation is ", theil_diff, "**.</span>")
   
   #DO THE PLOTTING! :)
-  p <- ggplot(combined_df, aes(x = iteration, y = theil_total)) + 
-            geom_line(aes(y = theil_overall, group = sim_id, color = theil_increased), alpha = 0.45) +
-            scale_color_manual(values = c("h_increased" = "steelblue4", "h_decreased" = "tomato")) + 
-            #geom_line(linewidth = 2, alpha = 0.75) +
-            geom_textpath(linewidth = 1.5,
-                           label = str_c("mean change: ", theil_diff), 
-                           stat = "smooth",
-                           color = "black",
-                           family = "Roboto Bold",
-                           size = 4,
-                           hjust = 0.25) +
+  p <- ggplot(combined_df, aes(x = iteration, y = theil_overall))
+  
+  if(plot_all_sims) {
+    p <- p +
+      geom_line(aes(y = theil_overall, group = sim_id, color = theil_increased), alpha = 0.35) +
+      scale_color_manual(values = c("h_increased" = "steelblue4", "h_decreased" = "tomato"))
+  } 
+  p <- p +  #stat_summary(geom = "smooth", 
+            #             fun.data = mean_cl_normal,
+            #             fun.args = list(conf.int = 0.95),
+            #             group = 1,
+            #             alpha = .5,
+            #             color = "black",
+            #             fill = "darkgrey",
+            #             se = TRUE) +
+            geom_smooth() + 
             labs(x = "Iteration", y = "Theil index", 
-                 title = "Theil over time",
-                 subtitle = str_c("Change in segregation between 100 organizations time with an agent (A/B) ratio of ", agent_ratio_str,
-                                  "\nThese are the results of ", length(batch), " simulations.")) +
-            theme_minimal() + 
-            theme(legend.position = "none",
-                  text = element_text(family = "Roboto"),
-                  plot.title = element_text(face = "bold",
-                                            size = rel(1.5)),
-                  axis.text = element_text(family = "Roboto", size = 10)) + 
-            ylim(0.025, 0.13)
+                 title = str_c("Between-organization segregation (Theil) change over time, ", pref_txt),
+                 subtitle = subtitle_str) + 
+            theme_minimal() 
   
   return(p)
 }
@@ -104,15 +119,22 @@ plot_batch_theil <- function(batch, agent_ratio_str) {
 
 # Saving for print and presentations - NEED WORK ------------------------------------------------------------------
 
-save_plot_print <- function(plot) {
+save_plot_print <- function(plot, name) {
   plot <- plot +     
-    theme(legend.position = "none",
-          text = element_text(family = "Roboto"),
-          plot.title = element_text(face = "bold",
-          size = rel(1.5)),
-          axis.text = element_text(family = "Roboto"))
+theme(legend.position = "none",
+      text = element_text(family = "Roboto"),
+      plot.title = element_text(face = "bold",
+                                size = 20,
+                                family = "Roboto"),
+      plot.subtitle = element_markdown(size = 16, family = "Roboto"),
+      axis.text = element_text(family = "Roboto", size = 14),
+      axis.title = element_text(size = 16))
   
-  ggsave()
+  ggsave(plot,
+         filename = str_c("plots/", name, ".png"),
+         device = "png", dpi = 300, units = "in",
+         height = 8.5,
+         width = 14)
 }
 
 save_plot_ppt <- function(plot) {
